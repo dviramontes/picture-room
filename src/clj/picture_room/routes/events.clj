7eb6a391cd2f8f0198edad096df5dbe03b7f071d
dviramontes <dviramontes@gmail.com>
2016-03-03 20:59:10 -0700

    simplifies response data

--- src/clj/picture_room/routes/events.clj
+++ src/clj/picture_room/routes/events.clj
@@ -3,13 +3,14 @@
     [compojure.core :refer [defroutes GET]]
     [environ.core :refer [env]]
     [fb-graph-clj.core :as fb]
+    ;; [ring.util.http-response :as res]
     [ring.util.response :refer [response]]))
 
 (def ac (or (env :access-token) (System/getenv "ACCESS_TOKEN")))
 
 (defn fb-events []
   (let [events (fb/with-access-token ac (fb/pull [:pictureroomnyc :events]))]
-    (response {:data (:data (:body events))})))
+    (response (:body events))))
 
 (defroutes event-routes
            (GET "/events" [] (fb-events)))
